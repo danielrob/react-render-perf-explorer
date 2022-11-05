25f805da721d234c5235347bbea373b167ed095b
Daniel Robinson <dan@danielrob.dev>
2022-11-05 22:15:08 +1300

    Add star

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GitHubButton from 'react-github-btn'
 import { AppScreen, AppComplexitySettings } from 'screens';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <section className="flex-1 p-5 border-l-slate-700 border-l-2">
         <AppComplexitySettings />
       </section>
+      <GitHubButton href="https://github.com/danielrob/react-render-perf-explorer" data-color-scheme="no-preference: dark_dimmed; light: light; dark: dark;"  data-size="large" aria-label="Star danielrob/react-render-perf-explorer on GitHub">&nbsp;Star</GitHubButton>
     </div>
   );
 }
